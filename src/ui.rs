505451762741aad4dd3610f412b836926c3f4bec
yor1xd <gabrielhenriquenf1@gmail.com>
2026-01-04 17:49:44 -0300

    add generic panel for testing

--- src/ui.rs
+++ src/ui.rs
@@ -2,3 +2,4 @@ pub mod pane;
 pub mod base;
 
 pub mod popup;
+pub mod generic_pane;
--- src/ui/base.rs
+++ src/ui/base.rs
@@ -1,18 +1,28 @@
 use std::time::Duration;
 
 use crossterm::event::{self, Event};
-use ratatui::{DefaultTerminal, Frame, layout::Rect, widgets::Widget};
+use ratatui::{DefaultTerminal, Frame, layout::{Constraint, Direction, Layout, Rect}, widgets::Widget};
 
-use crate::ui::{pane::Pane, popup::popup::FilePrompt};
+use crate::ui::{generic_pane::GenericPane, pane::Pane, popup::popup::FilePrompt};
 
 pub struct App {
+    pane_1: GenericPane,
+    pane_2: GenericPane,
+
     file_prompt: FilePrompt,
+
     exit: bool,
 }
+
+//Implement some type of Event Register
+//Implement some type of Window Register
 
 impl App {
     pub fn new() -> Self {
         Self { 
+            pane_1: GenericPane::new(),
+            pane_2: GenericPane::new(),
+
             file_prompt: FilePrompt::new(),
             exit: false, 
         }
@@ -49,6 +59,7 @@ impl App {
         match event::read() {
             Ok(Event::Key(ev)) => {
                 match ev.code { //Events
+                    //event::KeyCode::Tab => self.switch_window(),
                     event::KeyCode::Char('O') => self.file_prompt.toggle(),
                     event::KeyCode::Char('h') => self.file_prompt.pane.move_right(),
                     event::KeyCode::Char('j') => self.file_prompt.pane.move_down(),
@@ -57,7 +68,7 @@ impl App {
                     event::KeyCode::Char('q') => self.exit = true,
                     event::KeyCode::Char(c) => self.file_prompt.append(c),
                     event::KeyCode::Backspace => self.file_prompt.pop(),
-                    //self.event_handler.handle(active_window, char)
+                    //self.event_handler.handle(active_window, char),
                     _ => {},
                 }
             },
@@ -68,6 +79,16 @@ impl App {
 
 impl Widget for &mut App {
     fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer){
+        let split = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(vec![
+                Constraint::Percentage(50), 
+                Constraint::Percentage(50)])
+            .split(area);
+
+        self.pane_1.render(split[0], buf);
+        self.pane_2.render(split[1], buf);
+
         if self.file_prompt.is_visible() {
             let popup_area = Rect { x: 3*area.width/8, y: area.height/2 - 3, width: area.width/4, height: 3};
             self.file_prompt.render(popup_area, buf);
